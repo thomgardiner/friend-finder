c8e6e81414eda9c264798c5b3c9665f71431f15f
Thomas Gardiner <thomgardiner@gmail.com>
2018-06-04 12:29:25 -0600

    basic routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+let friends = require("./app/data/friends.js")
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  });
+

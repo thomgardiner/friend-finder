65ec63b1d2dc86b30e4cdb992175d55cfd8071c2
Thomas Gardiner <thomgardiner@gmail.com>
2018-06-05 14:21:08 -0600

    added more initial people objects to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,38 @@ const friends =[
         4,
         1
       ]
+  },
+  {
+    name:"Bob",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        3,
+        3,
+        5,
+        1,
+        2,
+        3,
+        4,
+        1
+      ]
+  },
+  {
+    name:"Steve",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
   }
 ]
   module.exports = friends;
